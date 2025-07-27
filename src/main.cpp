046c920e7f043b4703df2c8e6cdc6457ed26d5bf
Ekemini Udofia <ekeminiabasiudofia.com>
2025-07-27 22:52:45 +0100

    Added necessary submodules

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,26 @@
 #include <Arduino.h>
+#include <freertos/FreeRTOS.h>
+#include <freertos/task.h>
+#include <freertos/semphr.h>
 
-// put function declarations here:
-int myFunction(int, int);
+// Project Headers
+#include "FillLevelMonitor.hpp"
+#include "ProximityLidController.hpp"
+#include "SmartSortEngine.hpp"
+
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  // lid.begin();
+  // sorter.begin();
+  // binLevel.begin();
+
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  // lid.update();
+  // if (lid.lidJustClosed()) {
+    // sorter.processWaste();
+    // binLevel.checkLevel();
+  // }
 }
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
-}
