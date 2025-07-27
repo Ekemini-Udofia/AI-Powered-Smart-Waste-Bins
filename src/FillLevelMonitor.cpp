befd92a06bb0d614e30cde519e3b304bc991660f
Ekemini Udofia <ekeminiabasiudofia.com>
2025-07-27 23:57:21 +0100

    updates to FillLevelMonitor module

--- src/FillLevelMonitor.cpp
+++ src/FillLevelMonitor.cpp
@@ -1,8 +1,6 @@
 #include <Arduino.h>
 #include "config.hpp"
 
-//If Ultrasonic Sensor reads less than TRASH_FULL_LEVEL, do a loop to beep a buzzer on lid open
-
 void binInit()
 {
     pinMode(ULT_FILL_MONITOR_ECHO_PIN, INPUT);
@@ -14,14 +12,14 @@ bool binFull(){
     digitalWrite(ULT_FILL_MONITOR_TRIG_PIN, LOW);
     delayMicroseconds(2);
     digitalWrite(ULT_FILL_MONITOR_TRIG_PIN, HIGH);
-    delayMicroseconds(10); // Change this
+    delayMicroseconds(10); 
     digitalWrite(ULT_FILL_MONITOR_TRIG_PIN, LOW);
 
     float timer = pulseIn(ULT_FILL_MONITOR_ECHO_PIN, HIGH);
 
-    int trashLevel = (timer * SPEED_OF_SOUND_CM) / 2; //Replace with Speed of Sound Constant
+    int trashLevel = (timer * SPEED_OF_SOUND_CM) / 2; 
 
-    return trashLevel <= TRASH_FULL_LEVEL; // Causing the Buzzer to or to not beep at startup
+    return trashLevel <= TRASH_FULL_LEVEL;
 }
 
 
