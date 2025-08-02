5425f1e2d70e83936d65c5bcc4e8d4a6669f8d22
Ekemini-Udofia <ekeminiabasiudofia@gmail.com>
2025-08-02 21:07:57 +0100

    removed beep functionality

--- src/ProximityLidController.cpp
+++ src/ProximityLidController.cpp
@@ -37,20 +37,20 @@ void senseProximity() { // Checks if someone is close and opens the lid
     float timer = pulseIn(ULT_PROXIMITY_SENSOR_ECHO_PIN, HIGH);
     float humanProximity = (timer * SPEED_OF_SOUND_CM) / 2;
 
-    auto beep = []() -> void {
-        tone(BUZZER_PIN, 500);
-        delay(500);
-        noTone(BUZZER_PIN);
-        delay(500);
-        tone(BUZZER_PIN, 500);
-        delay(500);
-        noTone(BUZZER_PIN);
-    };
+    // auto beep = []() -> void {
+    //     tone(BUZZER_PIN, 500);
+    //     delay(500);
+    //     noTone(BUZZER_PIN);
+    //     delay(500);
+    //     tone(BUZZER_PIN, 500);
+    //     delay(500);
+    //     noTone(BUZZER_PIN);
+    // };
 
     if(humanProximity <= DIST_TO_OPEN_LID) {
         if(binFull) {
             // Beep Twice and don't open
-            beep();
+            // beep();
         } else {
             // Open Lid and Close after 10 Seconds
             openLid();
